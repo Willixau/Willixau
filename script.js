0f01415121fc0a2cf6ca8cec5a09bf3259c34493
Willixau <Willixau@outlook.com>
2026-06-13 18:32:15 -0400

    0.4.0

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const exploreBtn = document.getElementById('home-view');
 const backBtn = document.querySelector('.back-btn');
 const heroSection = document.querySelector('.hero');
 
+function updateBackBtnState() {
+    const isMenuVisible = wrapper.classList.contains('show-menu');
+    // Si isMenuVisible est vrai, on enlève 'disabled' (bouton actif).
+    // Si isMenuVisible est faux, on ajoute 'disabled' (bouton grisé).
+    backBtn.classList.toggle('disabled', !isMenuVisible);
+}
+
+updateBackBtnState();
+
 exploreBtn.addEventListener('click', () => {
     wrapper.classList.add('show-menu'); 
     heroSection.classList.add('hero-tall');
-    
-    // NOUVEAU : Ajoute #menu à l'URL
-    window.location.hash = "menu";
+    window.location.hash = "Menu";
+    updateBackBtnState();
 });
 
 backBtn.addEventListener('click', (event) => {
@@ -18,21 +26,23 @@ backBtn.addEventListener('click', (event) => {
     wrapper.classList.remove('show-menu'); 
     heroSection.classList.remove('hero-tall');
     
-    // NOUVEAU : Nettoie l'URL pour enlever le #menu
+    // NOUVEAU : Nettoie l'URL pour enlever le #Menu
     history.replaceState(null, null, ' ');
+    updateBackBtnState();
 });
 
 
 window.addEventListener('load', () => {
     // Si l'URL contient #menu, on déclenche l'animation directement
-    if (window.location.hash === "#menu") {
+    if (window.location.hash === "#Menu") {
         wrapper.classList.add('show-menu');
         heroSection.classList.add('hero-tall');
     }
+    updateBackBtnState();
 });
 
 window.addEventListener('hashchange', () => {
-    if (window.location.hash === "#menu") {
+    if (window.location.hash === "#Menu") {
         // Si l'URL devient #menu, on ouvre le panneau
         wrapper.classList.add('show-menu');
         heroSection.classList.add('hero-tall');
@@ -41,4 +51,5 @@ window.addEventListener('hashchange', () => {
         wrapper.classList.remove('show-menu');
         heroSection.classList.remove('hero-tall');
     }
+    updateBackBtnState();
 });
